c0c43afce0073fdb6601074722318313ad8f574c
svitlana1dev <ponomarchuk.svitlana.dev@gmail.com>
2024-05-15 20:41:59 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider } from "react-router-dom";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
+import { router } from "./router";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserAuthContextProvider>
+      <RouterProvider
+        router={router}
+        fallbackElement={<p>Loading...</p>}
+        context={{ isAuth: false }}
+      />
+    </UserAuthContextProvider>
   );
 }
 
